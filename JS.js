1d6d672e05876736c6aa692161c98a8112666253
אולגה מולינס <OLGA@wlghs-MacBook-Air.local>
2019-07-10 17:29:32 +0300

    cards recieve info from the api

--- JS.js
+++ JS.js
@@ -12,6 +12,8 @@ function populate() {
         li.innerHTML = parsed[i].name;
         li.addEventListener("click", function(e) {
           getCountry(e.target.textContent);
+          document.getElementById("myInput").value = "";
+          document.getElementById("myUL").style.display = "none";
         });
         document.getElementById("myUL").appendChild(li);
       }
@@ -53,10 +55,29 @@ function getCountry(value) {
     })
     .then(function(parsed) {
       var test = document.getElementById("test");
-
       var fromHTML = parsed[0].name;
-
       test.innerHTML = fromHTML;
+
+      var lang = document.getElementById("language");
+      var langApi = parsed[0].languages.map(function(element) {
+        return element.name;
+      });
+      lang.innerHTML = langApi;
+
+      var capi = document.getElementById("capital");
+      capi.innerHTML = parsed[0].capital;
+
+      var code = document.getElementById("code");
+      code.innerHTML = parsed[0].callingCodes;
+
+      var pop = document.getElementById("pop");
+      pop.innerHTML = parsed[0].population;
+
+      var currency = document.getElementById("currency");
+      currency.innerHTML = parsed[0].currencies[0].code;
+
+      var reg = document.getElementById("reg");
+      reg.innerHTML = parsed[0].region;
     })
     .catch(function(error) {
       console.log(error);
